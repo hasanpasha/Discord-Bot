a753285c6848781c4c95b19ac265a7f2fa9d363e
hasanpasha <bashahsn22@gmail.com>
2021-07-09 10:27:49 +0300

    set on_ready and handle errors

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,14 @@
-import asyncio
+from asyncio.tasks import Task
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from discord import (
+    Intents,
+    Embed,
+    File
+)
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import CommandNotFound
 
 PREFIX = "+"
 OWNER_IDS = [
@@ -13,7 +21,11 @@ class Bot(BotBase):
         self.ready = False
         self.guild = None
         self.scheduler = AsyncIOScheduler()
-        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+        super().__init__(
+            command_prefix=PREFIX,
+            owner_ids=OWNER_IDS,
+            Intents=Intents.all()
+        )
  
     def run(self, version):
         self.VERSION = version
@@ -30,11 +42,59 @@ class Bot(BotBase):
     async def on_disconnect(self):
         print("bot disconnected")
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Something went wrong.")
+
+        else:
+            stdout_channel = self.get_channel(862940566370254868)
+            await stdout_channel.send("An error occured.")
+
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            # await ctx.send("Wrong command.")
+            pass
+
+        elif hasattr(exc, "original"):
+            raise exc.original
+
+        else:
+            raise exc
+
+
     async def on_ready(self):
         if not self.ready:
             self.ready = True
-            self.guild = self.get_guild(853338000368599102)
             print("bot ready")
+
+            self.guild = self.get_guild(853338000368599102)
+            stdout_channel = self.get_channel(862940566370254868)
+            await stdout_channel.send("Now Online!")
+
+            embed = Embed(
+                title="Now Online!",
+                description="The server is now online",
+                colour=0xFF0000,
+                timestamp=datetime.utcnow()
+            )
+            fields = [
+                ("Owner", "Hasan Pasha", True),
+                ("Another field", "This field is next to each other", True),
+                ("A non-inline field", "This field will appear on it's own row.", False)
+            ]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="Demons Army", icon_url=self.guild.icon_url)
+            embed.set_footer(text="This is the footer.")
+            embed.set_image(url=self.guild.icon_url)
+            embed.set_thumbnail(url=self.guild.icon_url)
+
+            await stdout_channel.send(embed=embed)
+
+            await stdout_channel.send(file=File('./lib/db/data/images/avatar.png'))
 
         else:
             print("bot reconnected")
